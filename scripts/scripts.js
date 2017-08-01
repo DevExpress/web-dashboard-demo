09cbcede093381c3611ebce97b8f8586cc8885ab
Dmitry Rudnev <dmitry.rudnev@devexpress.com>
2017-08-01 15:40:00 +0300

    Add demo site

---
+++ scripts/scripts.js
@@ -0,0 +1,15 @@
+﻿function showDemoPopup() {
+    $("#demoInfoPopup").dxPopup({
+        visible: true,
+        animation: { hide: { type: 'pop', duration: 300, to: { opacity: 0, scale: 0.55 }, from: { opacity: 1, scale: 1 } } },
+        closeOnOutsideClick: true,
+        dragEnabled: false,
+        height: function() { return $(window).height(); },
+        position: { my: "right", at: "right", of: window },
+        showTitle: false,
+        width: "573px"
+    });
+}
+function hideDemoPopup() {
+    $("#demoInfoPopup").dxPopup('instance').hide();
+}
